ee2ec2b3254520cdb63e2bd2dc4e0f3166896f27
sijirama <gbemilesanmi@gmail.com>
2025-08-29 05:33:53 +0100

    gogle auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
-	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<div>Siji</div>
-		</div>
-	);
+import AuthPage from "@/components/AuthPage";
+import HomePage from "@/components/HomePage";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export default async function Home() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session?.user) {
+		return <AuthPage />;
+	}
+
+	return <HomePage />;
 }
